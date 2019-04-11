0866f3b4ad4ac9c91d3abaec16b7aba9ce122b18
Andrei Popovici <me@andreipopovici.co.uk>
2019-04-11 08:31:02 +0100

    Updated ApolloProvider setup

--- client/index.js
+++ client/index.js
@@ -5,32 +5,39 @@ import { Router, Route, Switch, IndexRoute } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 
 import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './components/App';
 import CompanyList from './components/CompanyList';
 import CompanyNew from './components/CompanyNew';
 // import SongList from './components/SongList';
 // import SongCreate from './components/SongCreate';
 // import SongDetail from './components/SongDetail';
 
-// const client = new ApolloClient({
-//   dataIdFromObject: o => o.id
-// });
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+})
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id,
+  cache,
+  link
+});
 
 
 const Root = () => {
   return (
-
-    <HashRouter>
-      <div className="container">
-        <Switch>
-          <Route exact path='/' component={CompanyList} />
-          <Route path='/company/new' component={CompanyNew} />
-        </Switch>
-      </div>
-    </HashRouter>
-
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={CompanyList} />
+            <Route path='/company/new' component={CompanyNew} />
+          </Switch>
+        </div>
+      </HashRouter>
+    </ApolloProvider>
   );
 };
 
